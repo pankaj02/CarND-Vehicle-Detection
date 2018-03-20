b9d16d5dee797c6c5c926381ae45d70da855ab8f
Pankaj Kumar <pankaj.kumar@iongroup.com>
2018-03-20 19:19:12 +0530

    Generated output image

--- yolo.py
+++ yolo.py
@@ -3,19 +3,22 @@ from keras import backend as K
 
 from bounding_box import Box
 from yad2k.models.keras_yolo import yolo_boxes_to_corners
-from yolo_utils import read_classes, read_anchors, preprocess_image, scale_boxes
+from utils import read_classes, read_anchors, preprocess_image, scale_boxes
 
 class_names = read_classes("model_data/coco_classes.txt")
 anchors = read_anchors("model_data/yolo_anchors.txt")
 
 
-def yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold=.6):
-    """Filters YOLO boxes by thresholding on object and class confidence.
+def filter_anchor_boxes(box_confidence, boxes, box_class_probs, threshold=.6):
+    """
+    Filters YOLO boxes by thresholding on object and class confidence
+    Filter any box for which the class "score" is less than a chosen threshold.
 
     Arguments:
-    box_confidence -- tensor of shape (19, 19, 5, 1)
-    boxes -- tensor of shape (19, 19, 5, 4)
-    box_class_probs -- tensor of shape (19, 19, 5, 80)
+
+    box_confidence -- tensor of shape (19, 19, 5, 1) - confidence probability that there's some object for each of the 5 boxes
+    boxes -- tensor of shape (19, 19, 5, 4) - (bx,by,bh,bw)  for each of the 5 boxes per cell.
+    box_class_probs -- tensor of shape (19, 19, 5, 80)- detection probabilities (c1,c2,...c80)  for each of the 80 classes for each of the 5 boxes per cell.
     threshold -- real value, if [ highest class probability score < threshold], then get rid of the corresponding box
 
     Returns:
@@ -25,19 +28,17 @@ def yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold=.6):
 
     """
 
-    # Step 1: Compute box scores
     box_scores = box_confidence * box_class_probs
 
-    # Step 2: Find the box_classes thanks to the max box_scores, keep track of the corresponding score
     box_classes = K.argmax(box_scores, axis=-1)  # returns index
     box_class_scores = K.max(box_scores, axis=-1, keepdims=False)  # returns max value
 
-    # Step 3: Create a filtering mask based on "box_class_scores" by using "threshold". The mask should have the
+    # Create a filtering mask based on "box_class_scores" by using "threshold". The mask should have the
     # same dimension as box_class_scores, and be True for the boxes you want to keep (with probability >= threshold)
     filtering_mask = tf.greater(box_class_scores,
                                 tf.constant(threshold))  # box_class_scores[box_class_scores < threshold]
 
-    # Step 4: Apply the mask to scores, boxes and classes
+    # Apply the mask to scores, boxes and classes
 
     scores = tf.boolean_mask(box_class_scores, filtering_mask)
     boxes = tf.boolean_mask(boxes, filtering_mask)
@@ -46,41 +47,14 @@ def yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold=.6):
     return scores, boxes, classes
 
 
-def iou(box1, box2):
-    """Implement the intersection over union (IoU) between box1 and box2
-
-    Arguments:
-    box1 -- first box, list object with coordinates (x1, y1, x2, y2)
-    box2 -- second box, list object with coordinates (x1, y1, x2, y2)
-    """
-
-    # Calculate the (y1, x1, y2, x2) coordinates of the intersection of box1 and box2. Calculate its Area.
-    xi1 = max(box1[0], box2[0])
-    yi1 = max(box1[1], box2[1])
-    xi2 = min(box1[2], box2[2])
-    yi2 = min(box1[3], box2[3])
-    inter_area = abs((yi2 - yi1) * (xi2 - xi1))
-
-    # Calculate the Union area by using Formula: Union(A,B) = A + B - Inter(A,B)
-
-    box1_area = abs((box1[1] - box1[3]) * (box1[0] - box1[2]))
-    box2_area = abs((box2[1] - box2[3]) * (box2[0] - box2[2]))
-    union_area = box1_area + box2_area - inter_area
-
-    # compute the IoU
-    iou = inter_area * 1.0 / union_area
-
-    return iou
-
-
-def yolo_non_max_suppression(scores, boxes, classes, max_boxes=10, iou_threshold=0.5):
+def non_max_suppression(scores, boxes, classes, max_boxes=10, iou_threshold=0.5):
     """
     Applies Non-max suppression (NMS) to set of boxes
 
     Arguments:
-    scores -- tensor of shape (None,), output of yolo_filter_boxes()
-    boxes -- tensor of shape (None, 4), output of yolo_filter_boxes() that have been scaled to the image size (see later)
-    classes -- tensor of shape (None,), output of yolo_filter_boxes()
+    scores -- tensor of shape (None,), output of filter_anchor_boxes()
+    boxes -- tensor of shape (None, 4), output of filter_anchor_boxes()
+    classes -- tensor of shape (None,), output of filter_anchor_boxes()
     max_boxes -- integer, maximum number of predicted boxes you'd like
     iou_threshold -- real value, "intersection over union" threshold used for NMS filtering
 
@@ -107,9 +81,9 @@ def yolo_non_max_suppression(scores, boxes, classes, max_boxes=10, iou_threshold
     return scores, boxes, classes
 
 
-def yolo_eval(yolo_outputs, image_shape=(720., 1280.), max_boxes=10, score_threshold=.6, iou_threshold=.5):
+def evaluate(yolo_outputs, image_shape=(720., 1280.), max_boxes=10, score_threshold=.5, iou_threshold=.5):
     """
-    Converts the output of YOLO encoding (a lot of boxes) to your predicted boxes along with their scores, box coordinates and classes.
+    Converts the output of YOLO encoding (yolo_outputs) to predicted boxes along with their scores, box coordinates and classes.
 
     Arguments:
     yolo_outputs -- output of the encoding model (for image_shape of (608, 608, 3)), contains 4 tensors:
@@ -117,8 +91,9 @@ def yolo_eval(yolo_outputs, image_shape=(720., 1280.), max_boxes=10, score_thres
                     box_xy: tensor of shape (None, 19, 19, 5, 2)
                     box_wh: tensor of shape (None, 19, 19, 5, 2)
                     box_class_probs: tensor of shape (None, 19, 19, 5, 80)
-    image_shape -- tensor of shape (2,) containing the input shape, in this notebook we use (608., 608.) (has to be float32 dtype)
-    max_boxes -- integer, maximum number of predicted boxes you'd like
+
+    image_shape -- tensor of shape (2,) original image shape
+    max_boxes -- integer, maximum number of predicted boxes
     score_threshold -- real value, if [ highest class probability score < threshold], then get rid of the corresponding box
     iou_threshold -- real value, "intersection over union" threshold used for NMS filtering
 
@@ -135,30 +110,32 @@ def yolo_eval(yolo_outputs, image_shape=(720., 1280.), max_boxes=10, score_thres
     boxes = yolo_boxes_to_corners(box_xy, box_wh)
 
     # Perform Score-filtering with a threshold of score_threshold
-    scores, boxes, classes = yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold=score_threshold)
+    scores, boxes, classes = filter_anchor_boxes(box_confidence, boxes, box_class_probs, threshold=score_threshold)
 
     # Scale boxes back to original image shape.
     boxes = scale_boxes(boxes, image_shape)
 
     # Non-max suppression with a threshold of iou_threshold
-    scores, boxes, classes = yolo_non_max_suppression(scores, boxes, classes, max_boxes, iou_threshold)
+    scores, boxes, classes = non_max_suppression(scores, boxes, classes, max_boxes, iou_threshold)
     return scores, boxes, classes
 
 
-def predict(sess, image, scores, boxes, classes, yolo_model):
+def predict_and_draw(sess, image, scores, boxes, classes, yolo_model):
     """
-    Runs the graph stored in "sess" to predict boxes for "image_file". Prints and plots the preditions.
+    Runs the graph stored in "sess" to predict boxes and draw bounding boxes
 
     Arguments:
     sess -- your tensorflow/Keras session containing the YOLO graph
-    image_file -- name of an image stored in the "images" folder.
+    image - Original Image
+    scores -- tensor of shape (None, ), predicted score for each box
+    boxes -- tensor of shape (None, 4), predicted box coordinates
+    classes -- tensor of shape (None,), predicted class for each box
+    yolo_model - pre-trained YOLO Model
+
 
     Returns:
-    out_scores -- tensor of shape (None, ), scores of the predicted boxes
-    out_boxes -- tensor of shape (None, 4), coordinates of the predicted boxes
-    out_classes -- tensor of shape (None, ), class index of the predicted boxes
+    image -- Image with bounding box drawn
 
-    Note: "None" actually represents the number of predicted boxes, it varies between 0 and max_boxes.
     """
 
     # Preprocess your image
@@ -170,7 +147,6 @@ def predict(sess, image, scores, boxes, classes, yolo_model):
                                                   feed_dict={yolo_model.input: image_data, K.learning_phase(): 0})
 
     # Draw bounding boxes on the image file
-    # draw_cv2_boxes(image, out_scores, out_boxes, out_classes, class_names)
     box = Box(image, out_scores, out_boxes, out_classes, class_names)
     box.draw_boxes()
 
--- yolo_pipeline.py
+++ yolo_pipeline.py
@@ -2,9 +2,10 @@ import cv2
 import matplotlib.pyplot as plt
 from keras import backend as K
 from keras.models import load_model
+import glob
 
 from yad2k.models.keras_yolo import yolo_head
-from yolo import yolo_eval, predict, class_names, anchors
+from yolo import evaluate, predict_and_draw, class_names, anchors
 
 sess = K.get_session()
 yolo_model = load_model("model_data/yolo.h5")
@@ -12,27 +13,16 @@ yolo_outputs = yolo_head(yolo_model.output, anchors, len(class_names))
 
 
 def pipeline(image):
-    scores, boxes, classes = yolo_eval(yolo_outputs)
-    image_pred = predict(sess, image, scores, boxes, classes, yolo_model)
+    scores, boxes, classes = evaluate(yolo_outputs)
+    image_pred = predict_and_draw(sess, image, scores, boxes, classes, yolo_model)
     return image_pred
 
 
 if __name__ == '__main__':
-    image_path = "test_images/test4.jpg"
+    images = glob.glob('test_images/test3*.jpg')
 
-    img = cv2.imread(image_path)
-    image = pipeline(img)
-
-    image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    f, ax = plt.subplots(1, 1, figsize=(15, 8))
-    # f, (ax1, ax2, ax3, ax4, ax5, ax6) = plt.subplot2grid(2, 3, figsize=(15, 8))
-    f.tight_layout()
-
-    ax.imshow(image)
-    ax.set_title('Drawn', fontsize=15)
-
-    print("")
-
-    # cv2.imshow("preview", image)
-    # cv2.waicv2.waitKey(500)
+    for image_path in images:
+        img = cv2.imread(image_path)
+        image = pipeline(img)
+        name = image_path.split("\\")[1].split('.')[0]+'.jpg'
+        cv2.imwrite('output_images/' + name, image)
